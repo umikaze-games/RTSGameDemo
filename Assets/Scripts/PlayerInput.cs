c2d68958c44a848dc633648f51ba85338f89eef4
umikaze-games <c463384962@gmail.com>
2025-11-01 17:21:33 +0900

    cinemachine control 01

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,79 @@
+using System;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+	[SerializeField] private Transform cameraTarget;
+	[SerializeField] private CinemachineCamera cinemachineCamera;
+	[SerializeField] private float moveSpeed = 5;
+	[SerializeField] private float zoomSpeed = 1;
+	[SerializeField] private float minZoomDistance = 7.5f;
+
+	private float zoomStartTime;
+	private Vector3 startingFollowOffset;
+	private CinemachineFollow cinemachineFollow;
+	private void Awake()
+	{
+		cinemachineFollow= cinemachineCamera.GetComponent<CinemachineFollow>();
+		startingFollowOffset= cinemachineFollow.FollowOffset;
+	}
+	private void Update()
+	{
+		HandlePanning();
+		HandleZooming();
+	}
+
+	private void HandleZooming()
+	{
+		if (ShouldUseZooming())
+		{
+			zoomStartTime = Time.time;
+		}
+		float zoomtime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+		Vector3 targetFollowOffset;
+		if (Keyboard.current.qKey.isPressed)
+		{
+			targetFollowOffset = new Vector3(
+				cinemachineFollow.FollowOffset.x,
+				minZoomDistance,
+				cinemachineFollow.FollowOffset.z);
+		}
+		else {
+			targetFollowOffset = new Vector3(
+			    cinemachineFollow.FollowOffset.x,
+				startingFollowOffset.y,
+				cinemachineFollow.FollowOffset.z);
+		}
+			cinemachineFollow.FollowOffset = Vector3.Slerp(cinemachineFollow.FollowOffset, targetFollowOffset, zoomtime);
+	}
+
+	private bool ShouldUseZooming()
+	{
+		return (Keyboard.current.qKey.wasPressedThisFrame || Keyboard.current.qKey.wasReleasedThisFrame);
+	}
+	private void HandlePanning()
+	{
+		Vector2 moveAmount = Vector2.zero;
+		if (Keyboard.current.upArrowKey.isPressed)
+		{
+			moveAmount.y += moveSpeed;
+		}
+		if (Keyboard.current.downArrowKey.isPressed)
+		{
+			moveAmount.y -= moveSpeed;
+		}
+		if (Keyboard.current.leftArrowKey.isPressed)
+		{
+			moveAmount.x -= moveSpeed;
+		}
+		if (Keyboard.current.rightArrowKey.isPressed)
+		{
+			moveAmount.x += moveSpeed;
+		}
+		moveAmount *= Time.deltaTime;
+		cameraTarget.position += new Vector3(moveAmount.x, 0, moveAmount.y);
+	}
+}
